b9b888b44837e08ded665a1a8dca541cb4319a96
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-12-01 13:49:33 +0530

    updated

--- src/ApiUrl.js
+++ src/ApiUrl.js
@@ -1,4 +1,4 @@
 
-export const baseURL = 'http://localhost:5001'
-// export const baseURL='https://billingapi.chanrericr.com'
+// export const baseURL = 'http://localhost:5001'
+export const baseURL='https://billingapi.chanrericr.com'
 // export const baseURL = 'https://billing-mair.onrender.com'
